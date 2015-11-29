da8f6c6a85032aeedd43b1a5befa87baa3766b2a
wuhaojie <w19961009@126.com>
2015-11-29 20:49:02 +0800

    complete UI.

--- app/src/main/java/com/voyager/logindemo/MainActivity.java
+++ app/src/main/java/com/voyager/logindemo/MainActivity.java
@@ -1,14 +1,37 @@
 package com.voyager.logindemo;
 
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn_register;
+    private Button btn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
     }
 
+    private void initView() {
+        btn_register = (Button) findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(this);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_register:
+                break;
+            case R.id.btn_login:
+                break;
+        }
+    }
 }
