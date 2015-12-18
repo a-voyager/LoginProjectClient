e69853138d95143debf2f63d9fda62ad22a5d521
wuhaojie <w19961009@126.com>
2015-12-18 22:40:20 +0800

    need to fix bug: OnClick() -- btn-- login (handler).

--- app/src/main/java/com/voyager/logindemo/MainActivity.java
+++ app/src/main/java/com/voyager/logindemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.voyager.logindemo;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,10 +18,15 @@ import org.apache.http.Header;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "Main";
+    private static final String HTTP_ADDR_LOGIN = "http://192.168.1.124:8080/LoginProject/servlet/Login";
+    private static final String HTTP_ADDR_REGISTER = "http://192.168.1.124:8080/LoginProject/servlet/Register";
     private Button btn_register;
     private Button btn_login;
     private EditText et_main_user;
     private EditText et_main_pwd;
+    private String userName;
+    private String userPwd;
+    private boolean result = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +51,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 register();
                 break;
             case R.id.btn_login:
-                login();
+                if (login()) {
+                    startActivity(new Intent(this, HomeActivity.class));
+                    finish();
+                }
                 break;
         }
     }
@@ -53,26 +62,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /**
      * 登录操作
      */
-    private void login() {
-
-    }
-
-    private void register() {
-        String userName = et_main_user.getText().toString();
-        String userPwd = et_main_pwd.getText().toString();
-        if (!Utils.isUserNameQualifiedRule(userName) || !Utils.isUserPwdQualifiedRule(userPwd)) {
-            Toast.makeText(this, "用户名或密码不合法", Toast.LENGTH_SHORT).show();
-            return;
-        }
+    private boolean login() {
+        if (!getEditTextUserInfo()) return false;
         RequestParams requestParams = new RequestParams();
         requestParams.add("user", userName);
         requestParams.add("pwd", userPwd);
-        Client.post(this, "http://192.168.1.117:8080/LoginProject/servlet/Register", requestParams, new AsyncHttpResponseHandler() {
+        Client.post(this, HTTP_ADDR_LOGIN, requestParams, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int i, Header[] headers, byte[] bytes) {
                 boolean flag = false;
                 for (Header header : headers) {
-                    Log.i(TAG, "-----" + header.getName() + "=" + header.getValue());
+                    Log.i(TAG, "login-----" + header.getName() + "=" + header.getValue());
+                    if ("result".equals(header.getName())) {
+                        if ("1".equals(header.getValue())) {
+                            flag = true;
+                        }
+                    }
+                }
+                if (flag) {
+                    Toast.makeText(MainActivity.this, "登录成功!", Toast.LENGTH_SHORT).show();
+                    result = true;
+                } else {
+                    Toast.makeText(MainActivity.this, "登录失败!", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+                Toast.makeText(MainActivity.this, "访问失败!", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        return result;
+    }
+
+    private void register() {
+        if (!getEditTextUserInfo()) return;
+        RequestParams requestParams = new RequestParams();
+        requestParams.add("user", userName);
+        requestParams.add("pwd", userPwd);
+        Client.post(this, HTTP_ADDR_REGISTER, requestParams, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Header[] headers, byte[] bytes) {
+                boolean flag = false;
+                for (Header header : headers) {
+                    Log.i(TAG, "register-----" + header.getName() + "=" + header.getValue());
                     if ("result".equals(header.getName())) {
                         if ("1".equals(header.getValue())) {
                             flag = true;
@@ -94,5 +129,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+    }
+
+    private boolean getEditTextUserInfo() {
+        userName = et_main_user.getText().toString();
+        userPwd = et_main_pwd.getText().toString();
+        if (!Utils.isUserNameQualifiedRule(userName) || !Utils.isUserPwdQualifiedRule(userPwd)) {
+            Toast.makeText(this, "用户名或密码不合法", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 }
